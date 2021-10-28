cf327d9a4fbfecb5b775930cd897dbde75204068
Corey Frenette <corey@narwhunderful.com>
2021-10-27 23:11:49 -0300

    Add a couple wrappers

--- src/main/java/legend/core/MemoryHelper.java
+++ src/main/java/legend/core/MemoryHelper.java
@@ -1,6 +1,8 @@
 package legend.core;
 
 import legend.core.memory.Method;
+import legend.core.memory.types.BiFunctionRef;
+import legend.core.memory.types.FunctionRef;
 
 public final class MemoryHelper {
   private MemoryHelper() { }
@@ -11,5 +13,13 @@ public final class MemoryHelper {
     } catch(final NoSuchMethodException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public static <T, U, R> BiFunctionRef<T, U, R> getBiFunctionAddress(final Class<?> cls, final String method, final Class<T> arg1, final Class<U> arg2, final Class<R> ret) {
+    return Hardware.MEMORY.ref(4, getMethodAddress(cls, method, arg1, arg2), BiFunctionRef::new);
+  }
+
+  public static <T, R> FunctionRef<T, R> getFunctionAddress(final Class<?> cls, final String method, final Class<T> arg, final Class<R> ret) {
+    return Hardware.MEMORY.ref(4, getMethodAddress(cls, method, arg), FunctionRef::new);
   }
 }
