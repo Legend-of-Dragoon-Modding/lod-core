0d699a294dfa65a14069f27148023c7f2e4727ac
Corey Frenette <corey@narwhunderful.com>
2022-08-13 15:45:29 -0300

    Don't try to copy pointers for array types

--- src/main/java/legend/core/MemoryHelper.java
+++ src/main/java/legend/core/MemoryHelper.java
@@ -2,10 +2,12 @@ package legend.core;
 
 import legend.core.memory.Method;
 import legend.core.memory.Value;
+import legend.core.memory.types.ArrayRef;
 import legend.core.memory.types.BiFunctionRef;
 import legend.core.memory.types.FunctionRef;
 import legend.core.memory.types.MemoryRef;
 import legend.core.memory.types.Pointer;
+import legend.core.memory.types.UnboundedArrayRef;
 
 import java.lang.reflect.Field;
 
@@ -38,6 +40,10 @@ public final class MemoryHelper {
           if(srcPtr.isNull()) {
             destPtr.clear();
           } else {
+            if(srcPtr.deref() instanceof ArrayRef<?> || srcPtr.deref() instanceof UnboundedArrayRef<?>) {
+              continue;
+            }
+
             destPtr.set(srcPtr.deref().getClass().getConstructor(Value.class).newInstance(Hardware.MEMORY.ref(4, destPtr.getPointer())));
 
             copyPointerTypes(destPtr.deref(), srcPtr.deref());
