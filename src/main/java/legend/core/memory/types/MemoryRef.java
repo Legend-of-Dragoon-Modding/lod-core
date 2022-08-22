39054119c3e3717abe4a29078285ff7c9492e10d
Corey Frenette <corey@narwhunderful.com>
2022-08-22 19:32:54 -0300

    Add bvec

--- src/main/java/legend/core/memory/types/MemoryRef.java
+++ src/main/java/legend/core/memory/types/MemoryRef.java
@@ -1,5 +1,14 @@
 package legend.core.memory.types;
 
+import legend.core.Hardware;
+import legend.core.memory.Value;
+
+import java.util.function.Function;
+
 public interface MemoryRef {
   long getAddress();
+
+  default <T extends MemoryRef> T reinterpret(final Function<Value, T> constructor) {
+    return constructor.apply(Hardware.MEMORY.ref(1, this.getAddress()));
+  }
 }
